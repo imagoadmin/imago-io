71fdd5769e4db28830af04e7e260bc5db08c8a5d
endevea <evans@endevea.com>
2017-08-11 12:43:40 +0800

    C# example

--- src/imago.io/Credentials.cs
+++ src/imago.io/Credentials.cs
@@ -9,18 +9,11 @@ namespace Imago.IO
 {
     public class Credentials
     {
-        public static string DefaultHostName { get; set; } = "http://imagomio:3000";
+        public static string DefaultHostName { get; set; }
         public static string DefaultApiVersion1 { get; protected set; } = "/integrate/1";
         public Credentials()
         {
-#if DEBUG
-                        HostName = "http://localhost:3000";
-            //HostName = "https://stagingapi.imago.live";
-            // HostName = Credentials.DefaultHostName;
-            //HostName = "https://portalapi.imago.live";
-#else
             HostName = "https://io.imago.live";
-#endif
             ApiVersion = Credentials.DefaultApiVersion1;
         }
         public string ProxyUri { get; set; } = null;
@@ -28,13 +21,5 @@ namespace Imago.IO
         public string ApiVersion { get; protected set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-
-        public bool IsDefined
-        {
-            get
-            {
-                return !String.IsNullOrWhiteSpace(UserName) && !String.IsNullOrWhiteSpace(Password) && !String.IsNullOrWhiteSpace(HostName);
-            }
-        }
     }
 }
