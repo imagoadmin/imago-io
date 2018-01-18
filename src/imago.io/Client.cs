295ee074a9291136d01c13398fa16adcdd722782
Phil Martin <phil@bitplex.com.au>
2018-01-18 10:42:55 +1000

    Add in a .Net Standard 2.0 project.

--- src/imago.io/Client.cs
+++ src/imago.io/Client.cs
@@ -10,11 +10,8 @@ using System.Net;
 using System.Web;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
-using System.Web.Script.Serialization;
-using System.Data.Entity.Design.PluralizationServices;
 using System.Globalization;
 using System.Threading;
-using System.Net.Http.Formatting;
 using System.Drawing;
 using System.Diagnostics;
 using System.IO;
@@ -33,8 +30,7 @@ namespace Imago.IO
         private CookieContainer _cookieJar;
         private JavaScriptSerializer _jsonConverter = new JavaScriptSerializer();
         private JsonSerializerSettings _jsonSettings = new JsonSerializerSettings();
- 
-        private PluralizationService _jsonPluralizer = PluralizationService.CreateService(CultureInfo.GetCultureInfo("en"));
+
         private HttpResponseMessage _lastResponse;
         private string _lastResponseBody;
 
@@ -62,7 +58,7 @@ namespace Imago.IO
             }
         }
 
-        public async Task<bool> SignIn(Credentials credentials)
+        public async Task<bool> SignIn(Credentials credentials, TimeSpan? timeout = null)
         {
             try
             {
@@ -77,19 +73,19 @@ namespace Imago.IO
 
                 HttpClientHandler responseHandler = null; ;
                 if (!String.IsNullOrWhiteSpace(credentials.ProxyUri))
-                    responseHandler = new HttpClientHandler { Proxy = new WebProxy(credentials.ProxyUri, false), UseProxy = true, UseDefaultCredentials=true };
+                    responseHandler = new HttpClientHandler { Proxy = new WebProxy(credentials.ProxyUri, false), UseProxy = true, UseDefaultCredentials = true };
                 else
                     responseHandler = new HttpClientHandler();
 
                 responseHandler.CookieContainer = _cookieJar;
 
                 _client = new HttpClient(responseHandler);
-                _client.Timeout = new TimeSpan(0, 10, 0);
+                _client.Timeout = timeout ?? new TimeSpan(0, 10, 0);
                 _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 Uri signInURI = new Uri(_apiUrl + "/session");
-                dynamic data = new { username=credentials.UserName, password=credentials.Password };
-                string body = _jsonConverter.Serialize(data);
+                var signindata = new { username = credentials.UserName, password = credentials.Password };
+                string body = _jsonConverter.Serialize(signindata, _jsonSettings);
                 HttpResponseMessage response = await _client.PutAsync(signInURI, new StringContent(body, Encoding.UTF8, "application/json")).ConfigureAwait(false);
 
                 _lastResponse = response;
@@ -102,8 +98,8 @@ namespace Imago.IO
                 if (response.StatusCode != HttpStatusCode.OK)
                     return false;
 
-                data = JObject.Parse(body);
-                string apiToken = data.apiToken;
+                var data = JObject.Parse(body);
+                string apiToken = (string)data["apiToken"];
 
                 _client.DefaultRequestHeaders.Add("imago-api-token", apiToken);
                 return true;
@@ -128,7 +124,7 @@ namespace Imago.IO
 
                 return (result.StatusCode == HttpStatusCode.OK);
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return false;
             }
@@ -159,6 +155,6 @@ namespace Imago.IO
         private string BuildQueryString(NameValueCollection nvc)
         {
             return string.Join("&", nvc.AllKeys.Where(key => !string.IsNullOrWhiteSpace(nvc[key])).Select(key => string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(nvc[key]))));
-        }    
+        }
     }
 }
