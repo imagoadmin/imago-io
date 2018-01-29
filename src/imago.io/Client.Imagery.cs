c4b8fb5e489aeb36617760b09853aca102175b59
Phil Martin <phil@bitplex.com.au>
2018-01-30 09:48:23 +1000

    Fixed incorrect formatting of error messages.

--- src/imago.io/Client.Imagery.cs
+++ src/imago.io/Client.Imagery.cs
@@ -127,7 +127,7 @@ namespace Imago.IO
                     _lastResponseBody = body;
 
                     Imagery dataItem = _jsonConverter.Deserialize<Imagery>(body);
-                    result = new Result<Imagery> { Value = dataItem, Code = dataItem == null || response.StatusCode != HttpStatusCode.OK ? ResultCode.failed : ResultCode.ok, Message = "{response.StatusCode} ${body}" };
+                    result = new Result<Imagery> { Value = dataItem, Code = dataItem == null || response.StatusCode != HttpStatusCode.OK ? ResultCode.failed : ResultCode.ok, Message = $"{response.StatusCode} {body}" };
                 }
             }
             catch(Exception ex)
